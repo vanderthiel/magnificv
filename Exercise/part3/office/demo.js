dbfabb0c30c52323f8cc8a90c87aa47dbeb01364
Edwin van der Thiel <edwin.vanderthiel@outlook.com>
2017-09-19 20:55:04 +0200

    Extra opdracht, om mee te spelen

---
+++ Exercise/part3/office/demo.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	// --------------------------------------------------------------------
+    // Schrijf hier je code
+
+
+
+    // --------------------------------------------------------------------
+});
+
+// Module Pattern
+/*
+	var MyModule = (function(){
+		...
+		return {result: ‘something’};
+	})();
+*/
+
+// Rectangle object definition
+/*
+	function Rectangle(x,y){
+		this.Width = x;
+		this.Height = y;
+		return this;
+	}
+	Rectangle.prototype.Surface = function(){
+		return this.Width * this.Height;
+	};
+*/
